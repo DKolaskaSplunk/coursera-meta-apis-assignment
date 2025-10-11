f44129e936e1308e7cc47fae132fc7f6257b608d
Damian Kolaska <dkolaska@splunk.com>
2025-10-11 19:17:50 +0200

    feat: create project and implement auth

---
+++ LittleLemon/LittleLemonAPI/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(
+    [
+        models.Category,
+        models.MenuItem,
+        models.Cart,
+        models.Order,
+        models.OrderItem,
+    ]
+)
