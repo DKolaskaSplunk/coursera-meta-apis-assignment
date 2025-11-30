ccf6432abd8de949a39b1c773f13528373984033
Damian Kolaska <dkolaska@splunk.com>
2025-11-30 15:23:36 +0100

    feat: add throttling

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -137,4 +137,9 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
     ],
     "DEFAULT_PAGINATION_CLASS": "LittleLemon.pagination.LittleLemonPagination",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "30/minute", "user": "100/minute"},
 }
